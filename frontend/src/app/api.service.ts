e84643929a8a69dfa28eccd1544824281980f1e0
Vincent De Borger <vincent.deborger@gmail.com>
2024-09-26 10:48:19 +0200

    chore: custom dev build configuration

--- frontend/src/app/api.service.ts
+++ frontend/src/app/api.service.ts
@@ -1,16 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  private apiUrl = 'http://localhost:8080/api/pods';  // Your Gin API URL
+  private baseUrl = environment.baseUrl;
+  private apiUrl = `${this.baseUrl}/pods`
 
   constructor(private http: HttpClient) {}
 
   getData(): Observable<any> {
+    console.log(this.apiUrl)
     return this.http.get<any>(this.apiUrl);
   }
 }
