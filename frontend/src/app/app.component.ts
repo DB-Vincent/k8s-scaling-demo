bab4fc5ad6e8d820af553ade7f4390caa43db4d7
Vincent De Borger <vincent.deborger@gmail.com>
2024-09-26 11:29:49 +0200

    feat: add autorefresh for data

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ApiService } from './api.service';
-import { OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,13 +10,23 @@ import { OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   replicas: any[] = [];
+  private intervalId: any;
+  private apiSubscription: Subscription = new Subscription();
 
   constructor(private apiService: ApiService) {}
 
   ngOnInit() {
-    this.apiService.getData().subscribe(
+    this.fetchData();
+
+    this.intervalId = setInterval(() => {
+      this.fetchData();
+    }, 5000);
+  }
+
+  private fetchData() {
+    const subscription = this.apiService.getData().subscribe(
       response => {
         this.replicas = response.replicas;
         console.log(this.replicas);
@@ -25,5 +35,15 @@ export class AppComponent implements OnInit {
         console.error('Error fetching data:', error);
       }
     );
+
+    this.apiSubscription.add(subscription);
+  }
+
+  ngOnDestroy() {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+
+    this.apiSubscription.unsubscribe();
   }
 }
