657becdb335fbd41a87b09711bea2147126a3dea
Vincent De Borger <vincent.deborger@gmail.com>
2024-09-26 11:39:45 +0200

    feat: instead of refreshing api call, refresh whole page to showcase loadbalancing

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ApiService } from './api.service';
-import { Subscription } from 'rxjs';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -10,23 +10,13 @@ import { Subscription } from 'rxjs';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, OnDestroy {
+export class AppComponent implements OnInit {
   replicas: any[] = [];
-  private intervalId: any;
-  private apiSubscription: Subscription = new Subscription();
 
   constructor(private apiService: ApiService) {}
 
   ngOnInit() {
-    this.fetchData();
-
-    this.intervalId = setInterval(() => {
-      this.fetchData();
-    }, 5000);
-  }
-
-  private fetchData() {
-    const subscription = this.apiService.getData().subscribe(
+    this.apiService.getData().subscribe(
       response => {
         this.replicas = response.replicas;
         console.log(this.replicas);
@@ -35,15 +25,5 @@ export class AppComponent implements OnInit, OnDestroy {
         console.error('Error fetching data:', error);
       }
     );
-
-    this.apiSubscription.add(subscription);
-  }
-
-  ngOnDestroy() {
-    if (this.intervalId) {
-      clearInterval(this.intervalId);
-    }
-
-    this.apiSubscription.unsubscribe();
   }
 }
